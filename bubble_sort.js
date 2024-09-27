363a960434c28f976760066e9bc2caaf713f92de
arsalanahmad123 <arsalanahmad15092003@gmail.com>
2024-09-27 23:44:51 +0500

    bubble sort

---
+++ bubble_sort.js
@@ -0,0 +1,19 @@
+function bubbleSort(arr) {
+    let n = arr.length;
+    let swapped = false;
+
+    for (let i = 0; i < n - 1; i++) {
+        for (let j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                [arr[j + 1], arr[j]] = [arr[j], arr[j + 1]];
+                swapped = true;
+            }
+        }
+
+        if (!swapped) break;
+    }
+    return arr;
+}
+
+let arr = [29, 10, 14, 37, 13];
+console.log(bubbleSort(arr));
